e47bfefe2dc7249420e8f0347b355c4d04fd59b9
Jackson <theminecoder@users.noreply.github.com>
2022-08-05 15:35:43 +1000

    accept * as subdomain name

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get('/check', function (req, res) {
 });
 
 function validateQuery(server) {
-    if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/.test(server.toLowerCase())) {
+    if (!/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9\*]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/.test(server.toLowerCase())) {
         return {
             success: false,
             message: "Invalid query!"
